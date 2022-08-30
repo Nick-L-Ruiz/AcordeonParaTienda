a342ad4d843571630562333f2b5bff3caa1b7b42
unknown <nicklom23@gmail.com>
2022-08-30 18:41:56 -0500

    acordeon para tienda online

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function mostrar() {
+    document.getElementById('CardContent').style.display = 'block';
+    document.getElementById('btnHidden').style.display = 'block';
+    document.getElementById('btnShow').style.display = 'none';
+    
+}
+function ocultar() {
+    document.getElementById('CardContent').style.display = 'none';
+    document.getElementById('btnHidden').style.display = 'none';
+    document.getElementById('btnShow').style.display = 'block';
+    
+}
